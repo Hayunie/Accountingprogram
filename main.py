bf302e16d9b359672fdcb51a6557ffa78f4d15f0
Julia <julia.svensson.park@gmail.com>
2022-12-30 10:47:27 +0100

    Outline of code

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
-print('testar')
+from openpyxl import *
+from tkinter import *
+
+
+wb = load_workbook("C:\\Users\\snoew\\Desktop\\demo.xlsx")
+
+sheet = wb.active
+
+# def excel():
+    # Set column dimensions
+
+    # Add Headers to top row
+
+# Set focus(event) for every field
+# def focus(event):
+
+#def clear():
+    # Clear every field in the GUI
+
+#def insert():
+    # Take the data from the GUI and write to excel file
+
+    # Get method to return current text as strinf and write it into excel
+    # at particular location
+
+    #wb.save("C:\\Users\\snoew\\Desktop\\demo.xlsx")
+
+    # set focus at first first field with focus_set()
+
+    # clear()
+
+# def newFile()
+    # add sheets for every month
+    # add all the headers
+
+# def openFile()
+
+# Driver code
+if __name__ == "__main__":
+    root = Tk()
+    # Set background color
+    # Set title of GUI Window
+    # Set the config of GUI window
+    # excel()
+
+    # Create labels for every data entry
+    # Grid method to place widgets at respective positions
+
+    # Create a text entrybox for every data entry
+    # Bind method to call for the focus function
+    # Grid method to place entry boxes
+
+    # excel()
+
+    # Save button
+
+    root.mainloop()
+
